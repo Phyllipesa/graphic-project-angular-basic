e28100576461b94e3fcd2d7bf5d338341b4b8333
Phyllipe <phyllipe.sjc@hotmail.com>
2024-09-04 17:20:34 -0300

    Atualizando imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from "./pages/home/home.component";
+import { NavbarComponent } from "./_components/navbar/navbar.component";
+import { FooterComponent } from "./_components/footer/footer.component";
+import { BaseUiComponent } from "./_components/base-ui/base-ui.component";
+import { BarComponent } from "./pages/bar/bar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HomeComponent, NavbarComponent, FooterComponent, BaseUiComponent, BarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
