b42ae71518787786ff6f08edf0177b3e774f135b
Phyllipe <phyllipe.sjc@hotmail.com>
2024-09-05 20:24:46 -0300

    Adicionado rota bar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { PieComponent } from './pages/pie/pie.component';
+import { BarComponent } from './pages/bar/bar.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
-    { path: 'pie', component: PieComponent }
+    { path: 'pie', component: PieComponent },
+    { path: 'bar', component: BarComponent }
 ];
