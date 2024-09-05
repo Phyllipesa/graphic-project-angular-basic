b1c7cb1fa89e64c442ed38dcca97f8d0f556403c
Phyllipe <phyllipe.sjc@hotmail.com>
2024-09-05 20:20:23 -0300

    Adicionado import do pieComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,12 +4,12 @@ import { HomeComponent } from "./pages/home/home.component";
 import { NavbarComponent } from "./_components/navbar/navbar.component";
 import { FooterComponent } from "./_components/footer/footer.component";
 import { BaseUiComponent } from "./_components/base-ui/base-ui.component";
-import { BarComponent } from "./pages/bar/bar.component";
+import { PieComponent } from './pages/pie/pie.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent, NavbarComponent, FooterComponent, BaseUiComponent, BarComponent],
+  imports: [RouterOutlet, HomeComponent, NavbarComponent, FooterComponent, BaseUiComponent, PieComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
