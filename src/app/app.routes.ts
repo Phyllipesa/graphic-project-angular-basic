042f29c7a0a50c470b5182223455a92d1780ed85
Phyllipe <phyllipe.sjc@hotmail.com>
2024-09-06 16:53:27 -0300

    Configurando rota line

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { PieComponent } from './pages/pie/pie.component';
 import { BarComponent } from './pages/bar/bar.component';
+import { LineComponent } from './pages/line/line.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'pie', component: PieComponent },
-    { path: 'bar', component: BarComponent }
+    { path: 'bar', component: BarComponent },
+    { path: 'line', component: LineComponent }
 ];
