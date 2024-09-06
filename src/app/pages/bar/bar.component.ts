a9dd953ee30903e665b4ef8d6924252157d53c96
Phyllipe <phyllipe.sjc@hotmail.com>
2024-09-05 22:22:36 -0300

    Configurando componente para receber as infos da api

--- src/app/pages/bar/bar.component.ts
+++ src/app/pages/bar/bar.component.ts
@@ -1,12 +1,67 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Data } from '../../_models/Data';
+import { VendasService } from '../../services/vendas.service';
+import { EChartsOption } from 'echarts'
+import { NgxEchartsDirective, provideEcharts } from 'ngx-echarts';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-bar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, NgxEchartsDirective],
   templateUrl: './bar.component.html',
-  styleUrl: './bar.component.css'
+  styleUrl: './bar.component.css',
+  providers: [
+    provideEcharts()
+  ]
 })
-export class BarComponent {
+export class BarComponent implements OnInit {
 
+  categories: string[]=[]; 
+  values: number[]=[]; 
+
+
+  constructor(private vendasService: VendasService) { }
+
+  ngOnInit(): void {
+    this.loadData();
+  }
+
+  chartOptions: EChartsOption = {};
+
+  loadData() {
+    this.vendasService.getGraphicInfo("toBar").subscribe((response) => {
+      this.categories = response.map((each) => each.name);
+      this.values = response.map((each) => each.value);
+      this.loadChartOptions();
+    });
+  }
+
+  loadChartOptions() {
+    this.chartOptions = {
+      xAxis: {
+        type: 'category',
+        data: this.categories,
+        axisLabel: {
+          interval: 45,
+          rotate: 0
+        }
+      },
+      yAxis: {
+        type: 'value'
+      },
+      series: [
+        {
+          name: 'Vendas (unit)',
+          data: this.values,
+          type: 'bar',
+          label: {
+            show: true,
+            position: 'top',
+            formatter: '{c} units'
+          }
+        }
+      ]
+    };
+  }
 }
