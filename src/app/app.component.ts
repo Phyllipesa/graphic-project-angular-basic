a39028432beae2a4b1c2e0fff22747fdff04e98f
Phyllipe <phyllipe.sjc@hotmail.com>
2024-09-06 16:54:10 -0300

    Adicionando lineComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,21 @@ import { FooterComponent } from "./_components/footer/footer.component";
 import { BaseUiComponent } from "./_components/base-ui/base-ui.component";
 import { PieComponent } from './pages/pie/pie.component';
 import { BarComponent } from './pages/bar/bar.component';
+import { LineComponent } from './pages/line/line.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent, NavbarComponent, FooterComponent, BaseUiComponent, PieComponent, BarComponent],
+  imports: [
+    RouterOutlet, 
+    HomeComponent, 
+    NavbarComponent, 
+    FooterComponent, 
+    BaseUiComponent, 
+    PieComponent, 
+    BarComponent,
+    LineComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
