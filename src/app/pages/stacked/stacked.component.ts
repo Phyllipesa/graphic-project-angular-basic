40da3b5e5cfdf17426b3e9f54128e444d82ddcb3
Phyllipe <phyllipe.sjc@hotmail.com>
2024-09-09 18:29:00 -0300

    Configuração do stackedComponent

--- src/app/pages/stacked/stacked.component.ts
+++ src/app/pages/stacked/stacked.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { Data } from '../../_models/Data';
 import { VendasService } from '../../services/vendas.service';
 import { EChartsOption } from 'echarts'
 import { CommonModule } from '@angular/common';
 import { NgxEchartsDirective, provideEcharts } from 'ngx-echarts';
+import { Stacked } from '../../_models/Stacked';
 
 @Component({
   selector: 'app-stacked',
@@ -16,24 +16,63 @@ import { NgxEchartsDirective, provideEcharts } from 'ngx-echarts';
   ]
 })
 export class StackedComponent implements OnInit {
+  
+  data!: Stacked;
+  chartOptions: EChartsOption = {};
 
-  data: Data[]=[]; 
-
-  constructor(private vendasService: VendasService) { }
+  constructor(private vendasService: VendasService) {}
 
   ngOnInit(): void {
     this.loadData();
   }
 
-  chartOptions: EChartsOption = {};
-
   loadData() {
-    this.vendasService.getGraphicInfo("toStacked").subscribe((response) => {
-      this.data = response;
-      this.loadCharOptions();
+    this.vendasService.getGraphicInfo<Stacked[]>("toStacked").subscribe((response) => {
+      if (response) {
+        this.data = response[0];
+        this.loadCharOptions();
+      }
     });
   }
 
   loadCharOptions() {
+    const { categorias, rawData, totalData } = this.data;
+    const series: echarts.BarSeriesOption[] = [
+        'Quantidade de Vendas',
+        'Quantidade de itens vendidos',
+    ].map((name, sid) => {
+        return {
+            name,
+            type: 'bar',
+            stack: 'total',  // Empilhar as barras
+            barWidth: '60%',
+            label: {
+                show: true,
+                formatter: (params: any) => Math.round(params.value * 1000) / 10 + '%'
+            },
+            data: rawData[sid].map((d, did) =>
+              totalData[did] <= 0 ? 0 : d / totalData[did])
+        };
+    });
+    
+    this.chartOptions = {
+      legend: {
+        selectedMode: false
+      },
+      grid: {
+        left: 100,
+        right: 50,
+        top: 50,
+        bottom: 50
+      },
+      xAxis: {
+          type: 'category',
+          data: categorias
+      },
+      yAxis: {
+          type: 'value'
+      },
+      series
+      };
   }
 }
