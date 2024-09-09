eb418a9e33c4e93d339103e9ef7a9e3a937f6c95
Phyllipe <phyllipe.sjc@hotmail.com>
2024-09-09 19:44:43 -0300

    Configurando rota Area

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { PieComponent } from './pages/pie/pie.component';
 import { BarComponent } from './pages/bar/bar.component';
 import { LineComponent } from './pages/line/line.component';
 import { StackedComponent } from './pages/stacked/stacked.component';
+import { AreaComponent } from './pages/area/area.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'pie', component: PieComponent },
     { path: 'bar', component: BarComponent },
     { path: 'line', component: LineComponent },
-    { path: 'stacked', component: StackedComponent }
+    { path: 'stacked', component: StackedComponent },
+    { path: 'area', component: AreaComponent },
 ];
